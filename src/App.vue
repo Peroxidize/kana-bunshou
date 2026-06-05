5f5902af2f23c35a55ee4e1d83b4ab5243135645
Peroxidize <marionemmanuelbuhat@gmail.com>
2026-06-05 18:05:18 +0800

    feat: PracticeArea dummy UI

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Header from "@/components/Header.vue";
+import PracticeArea from "@/components/PracticeArea.vue";
 </script>
 
 <template>
-  <main class="mx-auto my-8 max-w-5xl">
+  <main class="mx-auto my-12 max-w-5xl space-y-8 px-4">
     <Header />
+    <PracticeArea />
   </main>
 </template>
---
+++ src/components/PracticeArea.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+</script>
+
+<template>
+  <Card
+    class="flex min-h-100 flex-col items-center justify-between rounded-none!
+      px-8 py-4!"
+  >
+    <h3 class="text-muted-foreground text-2xl font-semibold">Write Hiragana</h3>
+    <span class="text-6xl">sha</span>
+    <Button class="rounded-none!">Reveal</Button>
+  </Card>
+</template>
